1623f5d24a35fb445e4a0f3b96e2a72d8a1d9616
Xzaton <piyushmishra20360@gmail.com>
2025-02-07 01:22:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,28 +8,7 @@ const messages = [
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! 💗❤️",
-    "Can I have you for the rest of my life",
-    "Say yes, Say yesss 😭😭",
-    "Cause i need to know...",
-    "Yeah no lyrics here, I made my own song hehe",
-    "Why you gotta be so ruude?", 
-    "Don't you know I am human tooo😭😭?",
-    "Why you gotta be so ruude?",
-    "I am gonna Marry You anyway..",
-    "Marry You girlll, Marry You Anyway",
-    "Marry You girll, Yeah no matter what you say",
-    "Marry You girllll",
-    "Say yes, Say yes pleasee!",
-    "Song ended, Now please say yes🥺🥺", 
-    "I will start crying now...",
-    "I really really will... 😭",
-    "You want me to see cry, Right?",
-    "So you really want, Don't you 😭", 
-    "I am cryingg Now 😭😭😭😭", 
-    "Ohh meri Husan Pari",
-    "Oh meri Pasandida Aurat",
-    "Ab toh Haa boldo na pleaseee! 💗❤️😭"
+    "Just kidding, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
@@ -37,27 +16,12 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    
-    // Update the text content of the "no-button"
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-
-    // Increase the font size of the "yes-button" by 1.5px
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize + 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
-
-    // Initialize the text of the "no-button"
-    noButton.textContent = "no"; // or set it to messages[0] if you want the first message initially
-
-    noButton.addEventListener('click', handleNoClick);
-    yesButton.addEventListener('click', handleYesClick);
-});
