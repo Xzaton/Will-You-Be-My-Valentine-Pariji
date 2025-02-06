00b94390238cdcfc1cebeb8bafc2117185b7007a
Xzaton <piyushmishra20360@gmail.com>
2025-02-07 02:39:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
+    "Are you positive?",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be very sad...",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! 💗❤️",
-    "Can I have you for the rest of my life",
-    "Say yes, Say yesss 😭😭",
-    "Cause i need to know...",
-    "Yeah no lyrics here, I made my own song hehe",
-    "Why you gotta be so ruude?", 
-    "Don't you know I am human tooo😭😭?",
-    "Why you gotta be so ruude?",
-    "I am gonna Marry You anyway..",
-    "Marry You girlll, Marry You Anyway",
-    "Marry You girll, Yeah no matter what you say",
-    "Marry You girllll",
-    "Say yes, Say yes pleasee!",
-    "Song ended, Now please say yes🥺🥺", 
-    "I will start crying now...",
-    "I really really will... 😭",
-    "You want me to see cry, Right?",
-    "So you really want, Don't you 😭", 
-    "I am cryingg Now 😭😭😭😭", 
-    "Ohh meri Husan Pari",
-    "Oh meri Pasandida Aurat",
-    "Ab toh Haa boldo na pleaseee! 💗❤️😭",
-    "Ab toh End agya",
-    "Ek per pe khde hoke, rotte hue bol raha😭",
-    "Pleaseee say Yes😭😭💗❤️ manjao😭"
+    "Just kidding, say yes please! ❤️"
 ];
+
 let messageIndex = 0;
 
 function handleNoClick() {
